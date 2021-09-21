9cfc569a357a13255e393f31fd71995c5bdb01ee
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-09-21 18:22:51 -0300

    Configurando as views e adicionando database

--- dbConnection.js
+++ dbConnection.js
@@ -2,5 +2,5 @@ const { Pool } = require('pg')
 
 
 const client = new Pool({
-    connectionString: 'postgres://zbgytapkxvxcsx:5dec7cba393824256a23d8ced44f7db6d4b0ad30d382bf35254d4936ee2b4fcd@ec2-3-219-111-26.compute-1.amazonaws.com:5432/de867150eoetc5'
+    connectionString: ''
 })
